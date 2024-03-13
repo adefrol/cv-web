aa2aa576dc5ac06f5f6bee5dcb35521fb9e109a3
3N14C <3n14c@vk.com>
2024-03-13 13:16:22 +0800

    Update index.tsx

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -1,33 +1,47 @@
 import { PageTitle } from "@/components/PageTitle";
 import { IMAGE_URL } from "@/constants/images";
-import { useSwitchUser } from "@/context/SwitchUserContext";
-import { createFileRoute } from "@tanstack/react-router";
+import { projects } from "@/lib/projects";
+import { Link, createFileRoute } from "@tanstack/react-router";
+import { MoveUpRight } from "lucide-react";
 
 export const Route = createFileRoute("/projects/")({
   component: () => <Projects />,
 });
 
 const Projects = () => {
-  const { user } = useSwitchUser();
-
   return (
     <>
       <PageTitle title="Проекты" />
 
-      <div className="">
-        <div className="">
-          {user?.resume.map((resume) =>
-            resume.projects.length ? (
-              resume.projects.map((project) => (
-                <div className="" key={project.id}>
-                  <p className="">{project.title}</p>
-                </div>
-              ))
-            ) : (
-              <div className="flex flex-col items-center" key={resume.id}>
-                <img src={IMAGE_URL[404]} className="h-screen" alt="" />
-              </div>
-            )
+      <div className="mt-[50px]">
+        <div className="grid grid-cols-3 lg:gap-[60px] max-[639px]:flex max-[639px]:flex-col max-[639px]:gap-[40px]">
+          {projects.length ? (
+            projects.map((project) => (
+              <Link
+                to="/projects/$projectId"
+                params={{ projectId: project.id.toString() }}
+                key={project.id}
+                className="flex flex-col gap-[5px] max-[639px]:gap-0"
+              >
+                <img
+                  src={project.img}
+                  className="w-[350px] h-[200px] max-[639px]:h-full max-[639px]:w-full"
+                />
+
+                <p className="text-black/50 font-semibold">
+                  {project.dateTime}
+                </p>
+
+                <p className="font-bold text-[25px] flex items-center gap-1">
+                  {project.title}
+                  <MoveUpRight size={20} />
+                </p>
+              </Link>
+            ))
+          ) : (
+            <div className="flex flex-col items-center">
+              <img src={IMAGE_URL[404]} className="h-screen" alt="" />
+            </div>
           )}
         </div>
       </div>
