9afcbdcc0f58a8465879f4acbb0b4a6985862293
3N14C <3n14c@vk.com>
2024-03-13 19:07:06 +0800

    Update Education.tsx

--- src/components/resume/Education.tsx
+++ src/components/resume/Education.tsx
@@ -1,16 +1,28 @@
 import { IUser } from "@/interfaces/user.interface";
 import { FC } from "react";
 
-export const Education: FC<{ user: IUser }> = ({user}) => {
-  return <div className="">
-    {user?.resume.map((item) => (
-      <div className="" key={item.id}>
-        {item.education.map((edu) => (
-          <div className="" key={edu.id}>
-            {edu.title}
-          </div>
-        ))}
-      </div>
-    ))}
-  </div>;
+export const Education: FC<{ user: IUser }> = ({ user }) => {
+  return (
+    <div className="">
+      {user?.resume.map((item) => (
+        <div className="" key={item.id}>
+          {item.education.map((edu) => (
+            <div className="space-y-[5px]" key={edu.id}>
+              <p className="text-black/50 font-semibold">{edu.dateTime}</p>
+
+              <p className="font-bold">
+                {edu.educationType}:{" "}
+                <span className="font-normal">{edu.educationPlace}</span>
+              </p>
+
+              <p className="font-bold">
+                Специальность:{" "}
+                <span className="font-normal">{edu.profession}</span>
+              </p>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
