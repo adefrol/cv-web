990c4cb024654b0675cb202878df0a07398c1ee6
3N14C <3n14c@vk.com>
2024-03-13 13:10:25 +0800

    Update Contacts.tsx
    reworked component

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,24 +1,67 @@
-import { useSwitchUser } from "@/context/SwitchUserContext";
-import { FC } from "react";
-import { Button } from "./ui/button";
+import { IMAGE_URL } from "@/constants/images";
 import { MoveUpRight } from "lucide-react";
+import { FC } from "react";
+import tg from "../assets/tg.svg";
+import vk from "../assets/vk.svg";
+import { FormComponent } from "./contacts/form/FormComponent";
+import { SocialIcon } from "./contacts/SocialIcon";
+import { Button } from "./ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
 
 export const Contacts: FC = () => {
-  const { user } = useSwitchUser();
-
   return (
-    <div className="flex items-center gap-[20px] max-[639px]:flex-col">
-      {user?.contact.map((item) => (
-        <div key={item.id} className="">
-          <Button className="rounded-full" size={"lg"}>
-            <a className="" target="_blank" href={item.href}>
-              <p className="flex gap-[10px] items-center">
-                {item.title} <MoveUpRight size={15} />
-              </p>
-            </a>
+    <div className="flex items-center gap-[20px] max-[639px]:flex-col mt-[20px]">
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button
+            className="rounded-full flex gap-[10px] items-center"
+            size={"lg"}
+          >
+            Связаться с нами <MoveUpRight size={15} />
           </Button>
-        </div>
-      ))}
+        </DrawerTrigger>
+
+        <DrawerContent
+          style={{ backgroundImage: `url(${IMAGE_URL.bg})` }}
+          className="py-[40px] bg-cover"
+        >
+          <div className="mx-auto w-full max-w-sm">
+            <DrawerHeader className="mt-[10px]">
+              <DrawerTitle className="text-center text-3xl">
+                Связаться с нами
+              </DrawerTitle>
+
+              <DrawerDescription className="text-center font-semibold">
+                Выберите один из способов связи
+              </DrawerDescription>
+            </DrawerHeader>
+
+            <div className="p-4 pb-0 space-y-4">
+              <FormComponent />
+
+              <div className="flex justify-center items-center gap-2">
+                <div className="w-full h-[0.5px] bg-black/50" />
+                <p className="uppercase text-center text-black/50">или</p>
+                <div className="w-full h-[0.5px] bg-black/50" />
+              </div>
+
+              <div className="flex items-center justify-center">
+                <div className="flex items-center gap-5">
+                  <SocialIcon icon={tg} />
+                  <SocialIcon icon={vk} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </DrawerContent>
+      </Drawer>
     </div>
   );
 };
