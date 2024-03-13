458ab5bcc14b0352f2529e8e69bc1c68dc264256
3N14C <3n14c@vk.com>
2024-03-13 18:47:40 +0800

    Update index.tsx

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -14,33 +14,40 @@ const Projects = () => {
       <PageTitle title="Проекты" />
 
       <div className="mt-[50px]">
-        <div className="grid grid-cols-3 lg:gap-[60px] max-[639px]:flex max-[639px]:flex-col max-[639px]:gap-[40px]">
-          {projects.length ? (
-            projects.map((project) => (
-              <Link
-                to="/projects/$projectId"
-                params={{ projectId: project.id.toString() }}
-                key={project.id}
-                className="flex flex-col gap-[5px] max-[639px]:gap-0"
-              >
-                <img
-                  src={project.img}
-                  className="w-[350px] h-[200px] max-[639px]:h-full max-[639px]:w-full"
-                />
+        <div className="">
+          {!projects.length ? (
+            <div className="grid grid-cols-3 lg:gap-[60px] max-[639px]:flex max-[639px]:flex-col max-[639px]:gap-[40px]">
+              {projects.map((project) => (
+                <Link
+                  to="/projects/$projectId"
+                  params={{ projectId: project.id.toString() }}
+                  key={project.id}
+                  className="flex flex-col gap-[5px] max-[639px]:gap-0"
+                >
+                  <img
+                    src={project.img}
+                    className="w-[350px] h-[200px] max-[639px]:h-full max-[639px]:w-full"
+                  />
 
-                <p className="text-black/50 font-semibold">
-                  {project.dateTime}
-                </p>
+                  <p className="text-black/50 font-semibold">
+                    {project.dateTime}
+                  </p>
 
-                <p className="font-bold text-[25px] flex items-center gap-1">
-                  {project.title}
-                  <MoveUpRight size={20} />
-                </p>
-              </Link>
-            ))
+                  <p className="font-bold text-[25px] flex items-center gap-1">
+                    {project.title}
+                    <MoveUpRight size={20} />
+                  </p>
+                </Link>
+              ))}
+            </div>
           ) : (
-            <div className="flex flex-col items-center">
-              <img src={IMAGE_URL[404]} className="h-screen" alt="" />
+            <div className="text-center">
+              <p
+                className="text-3xl bg-gradient-to-r from-violet-500 via-pink-500 to-fuchsia-500 inline-block 
+              bg-clip-text text-transparent font-bold"
+              >
+                Данная страница пока находится в разработке
+              </p>
             </div>
           )}
         </div>
