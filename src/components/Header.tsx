17637a680cfa092daac73fa8197f829e5650b789
3N14C <3n14c@vk.com>
2024-03-13 18:01:04 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Link, LinkProps, useRouter } from "@tanstack/react-router";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { users } from "@/lib/users";
 import { useSwitchUser } from "@/context/SwitchUserContext";
@@ -14,7 +14,7 @@ interface INavbar {
 const navbar: INavbar[] = [
   {
     id: 1,
-    title: "О себе",
+    title: "О нас",
     href: "/",
   },
 
@@ -31,31 +31,49 @@ const navbar: INavbar[] = [
   },
 ];
 
-export const Header: FC = () => {
+interface IProps {}
+
+export const Header: FC<IProps> = ({}) => {
   const { switchUser, user: currentUser } = useSwitchUser();
   const { latestLocation } = useRouter();
   const [focus, setFocus] = useState(latestLocation.pathname);
+  const [logo, setLogo] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (latestLocation.pathname === "/resume") {
+      setLogo(false);
+    }
+    return () => setLogo(true);
+  }, [latestLocation.pathname]);
 
   return (
     <div className="flex items-center justify-between max-[639px]:flex-col max-[639px]:gap-[30px]">
       <div className="flex items-center gap-[30px] max-[639px]:gap-[50px]">
-        {users.map((user) => (
-          <div
-            key={user.id}
-            onClick={() => switchUser(user.id)}
-            className={cn("flex flex-col items-center opacity-50", {
-              "opacity-100 transition duration-300":
-                user.id === currentUser?.id,
-            })}
-          >
-            <Avatar className="cursor-pointer w-[50px] h-[50px] max-[639px]:w-[60px] max-[639px]:h-[60px]">
-              <AvatarImage src={user.img} />
-              <AvatarFallback>{user.name}</AvatarFallback>
-            </Avatar>
-
-            <p className="">{user.name}</p>
+        {logo ? (
+          <div className="">
+            <p className="text-3xl">LOGO</p>
           </div>
-        ))}
+        ) : (
+          <>
+            {users.map((user) => (
+              <div
+                key={user.id}
+                onClick={() => switchUser(user.id)}
+                className={cn("flex flex-col items-center opacity-50", {
+                  "opacity-100 transition duration-300":
+                    user.id === currentUser?.id,
+                })}
+              >
+                <Avatar className="cursor-pointer w-[50px] h-[50px] max-[639px]:w-[60px] max-[639px]:h-[60px]">
+                  <AvatarImage src={user.img} />
+                  <AvatarFallback>{user.name}</AvatarFallback>
+                </Avatar>
+
+                <p className="">{user.name}</p>
+              </div>
+            ))}
+          </>
+        )}
       </div>
 
       <div className="bg-white py-[10px] px-[20px] rounded-full w-fit flex gap-[40px] items-center shadow-[#7266e26a] max-[639px]:shadow-[#efdae6] shadow-xl">
